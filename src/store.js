277bf6ed055513c7a564f3dff949a49d4b42fa46
Raghu <raghujm0291@gmail.com>
2023-04-03 11:03:29 +0530

    first commit new

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./services/authApi";
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
   },
+  devTools: process.env.NODE_ENV === 'development',
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware),
 });
